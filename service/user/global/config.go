0fb6989670ad5fe61d917a04ab7d36199a31f078
weiweimeng <mengweiwei@soyoung.com>
2021-11-04 10:04:24 +0800

    更新代码

--- service/user/global/config.go
+++ service/user/global/config.go
@@ -21,4 +21,12 @@ type ServiceConfig struct {
 	Consul ConsulConfig `mapstruct:"consul" json:"consul"`
 }
 
+type NacosConfig struct {
+	IP          string `mapstructure:"ip" json:"ip"`
+	Port        int    `mapstructure:"port" json:"port"`
+	NamespaceId string `mapstructure:"namespaceId" json:"namespaceId"`
+	DataId      string `mapstructure:"dataId" json:"dataId"`
+	Group       string `mapstructure:"group" json:"group"`
+}
+
 var Config *ServiceConfig
--- service/user/main.go
+++ service/user/main.go
@@ -1,19 +1,20 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/ervin-meng/go-stitch-monster/infrastructure/event"
 	"github.com/ervin-meng/go-stitch-monster/infrastructure/middleware/logger"
-	"github.com/ervin-meng/go-stitch-monster/infrastructure/middleware/register"
 	"github.com/ervin-meng/go-stitch-monster/infrastructure/middleware/tracer"
 	"github.com/fsnotify/fsnotify"
+	"github.com/nacos-group/nacos-sdk-go/clients"
+	"github.com/nacos-group/nacos-sdk-go/common/constant"
+	"github.com/nacos-group/nacos-sdk-go/vo"
 	"github.com/spf13/viper"
 	"go-project/common/proto"
 	"go-project/service/user/domain/server"
 	"go-project/service/user/global"
 	"google.golang.org/grpc"
-	"google.golang.org/grpc/health"
-	"google.golang.org/grpc/health/grpc_health_v1"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 	gormlogger "gorm.io/gorm/logger"
@@ -30,7 +31,8 @@ func main() {
 	//初始化日志中间件
 	logger.Init()
 	//初始化配置
-	InitConfig()
+	InitConfigWithCenter()
+	//InitConfig()
 	//初始化数据库
 	InitDb()
 	//初始化链路追踪中间件
@@ -44,9 +46,9 @@ func main() {
 	//注册用户服务
 	proto.RegisterUserServer(RpcServer, &server.UserServer{})
 	//注册健康检查接口
-	grpc_health_v1.RegisterHealthServer(RpcServer, health.NewServer())
+	//grpc_health_v1.RegisterHealthServer(RpcServer, health.NewServer())
 	//注册到服务发现中心
-	register.Init(register.HTTPService, global.Config.Name, global.Config.Consul.IP, global.Config.Consul.Port)
+	//register.Init(register.HTTPService, global.Config.Name, global.Config.Consul.IP, global.Config.Consul.Port)
 	//监听服务
 	lis, err := net.Listen("tcp", fmt.Sprintf("%s:%d", "0.0.0.0", port))
 	if err != nil {
@@ -70,6 +72,74 @@ func main() {
 
 	//触发服务退出事件
 	event.Trigger(event.ServiceTerm)
+}
+
+func InitConfigWithCenter() {
+	viper.AutomaticEnv()
+
+	configFIleEnv := viper.GetString("API_ENV")
+
+	if configFIleEnv == "" {
+		configFIleEnv = "pro"
+	}
+
+	configFileName := fmt.Sprintf("infrastructure/config/nacos-%s.yml", configFIleEnv)
+
+	v := viper.New()
+
+	v.SetConfigFile(configFileName)
+
+	if err := v.ReadInConfig(); err != nil {
+		panic(err)
+	}
+
+	nacosConfig := global.NacosConfig{}
+
+	if err := v.Unmarshal(&nacosConfig); err != nil {
+		panic(err)
+	}
+
+	sc := []constant.ServerConfig{
+		{
+			IpAddr: nacosConfig.IP,
+			Port:   uint64(nacosConfig.Port),
+		},
+	}
+
+	cc := constant.ClientConfig{
+		NamespaceId:         nacosConfig.NamespaceId,
+		TimeoutMs:           5000,
+		NotLoadCacheAtStart: true,
+		LogDir:              "config/tmp/nacos/log",
+		CacheDir:            "config/tmp/nacos/cache",
+		RotateTime:          "1h",
+		MaxAge:              3,
+		LogLevel:            "debug",
+	}
+
+	client, _ := clients.CreateConfigClient(map[string]interface{}{
+		"serverConfigs": sc,
+		"clientConfig":  cc,
+	})
+
+	content, _ := client.GetConfig(vo.ConfigParam{
+		DataId: nacosConfig.DataId,
+		Group:  nacosConfig.Group,
+	})
+
+	_ = json.Unmarshal([]byte(content), &global.Config)
+
+	go func() {
+		_ = client.ListenConfig(vo.ConfigParam{
+			DataId: nacosConfig.DataId,
+			Group:  nacosConfig.Group,
+			OnChange: func(namespace, group, dataId, data string) {
+				_ = json.Unmarshal([]byte(data), &global.Config)
+				logger.Global.Info("配置中心文件更新")
+				logger.Global.Info(global.Config)
+			},
+		})
+	}()
 }
 
 func InitConfig() {
